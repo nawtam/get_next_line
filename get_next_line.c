f2c2a5a2adb665dc493a261c9e6ae8bee9106d47
Nawel Tamacha <ntamacha@f2r8s12.clusters.42paris.fr>
2025-05-27 14:25:15 +0200

    fonctionnel to do ; gerer les leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,31 @@
 /*   By: ntamacha <ntamacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 10:39:39 by ntamacha          #+#    #+#             */
-/*   Updated: 2025/05/26 16:43:44 by ntamacha         ###   ########.fr       */
+/*   Updated: 2025/05/27 14:23:48 by ntamacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_statictmp(char *buf, int i, int bytes)
+{
+	char	*tmp;
+	int		j;
+
+	j = 0;
+	i++;
+	tmp = malloc(bytes - i + 1);
+	if (!tmp)
+		return (NULL);
+	while (i <= (bytes - 1))
+	{
+		tmp[j] = buf[i];
+		i++;
+		j++;
+	}
+	tmp[j] = '\0';
+	return (tmp);
+}
 
 char	*get_next_line(int fd)
 {
@@ -20,11 +40,7 @@ char	*get_next_line(int fd)
 	char			buf[BUFFER_SIZE + 1];
 	int 			i;
 
-	if (tmp)
-	{
-		line = tmp;
-		tmp = NULL;
-	}
+	line = tmp;
 	while (1)
 	{
 		i = 0;
@@ -36,14 +52,16 @@ char	*get_next_line(int fd)
 			line = ft_strjoin(line, buf[i]);
 			if (buf[i] == '\n')
 			{
-				while (i++ <= (bytes - 1))
-					tmp = ft_strjoin(tmp, buf[i++]);
+				tmp = (ft_statictmp(buf, i, bytes));
 				return (line);
 			}
 			i++;
 		}
-
+	}
+	free (line);
+	return (NULL);
 }
+
 
 #include <fcntl.h>
 #include <unistd.h>
@@ -55,11 +73,15 @@ int main()
 
 	line = get_next_line(fd);
 	printf("%s", line);
-	while (line)
-	{
-		line = get_next_line(fd);
-		printf("%s", line);
-	}
+	free (line);
+	// while (1)
+	// {
+	// 	line = get_next_line(fd);
+	// 	if (!line)
+	// 		break ;
+	// 	printf("%s", line);
+	// 	free (line);
+	// }
 	close(fd);
 	return (0);
 }
